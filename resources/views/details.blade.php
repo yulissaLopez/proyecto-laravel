aced77415317bc94c540998963db0d4b6a2374d2
Kevin A. Diaz <kevinalextor@gmail.com>
2024-11-22 14:52:38 -0500

    assets y archivos html

---
+++ resources/views/details.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html data-whatinput="keyboard" data-whatintent="keyboard" class=" whatinput-types-initial whatinput-types-keyboard"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
+        <title>PHP MVC Frameworks</title>
+            <link rel="icon" type="image/x-icon" href="favicon.ico" />
+            <link rel="stylesheet" href="./assets/css/foundation.css">
+            <link rel="stylesheet" href="./assets/css/app.css">
+    <meta class="foundation-mq"></head>
+    <body>
+
+        <!-- Start Top Bar -->
+    <div class="top-bar">
+      <div class="row">
+        <div class="top-bar-left">
+          <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
+            <li role="menuitem">PHP MVC Frameworks</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+    <br>
+    
+    
+    <div class="row">
+      <div class="medium-12 large-12 columns">
+        <h4>Details</h4>
+        <form method="POST">
+          <div class="medium-4  columns">
+            <label>Name</label>
+            <input type="text" name="name">
+          </div>
+          <div class="medium-4  columns">
+            <label>Last Name</label>
+            <input type="text" name="last_name">
+          </div>
+          <div class="medium-4  columns">
+            <label>Email</label>
+            <input type="text" name="email">
+          </div>
+          <div class="medium-12  columns">
+            <input value="SEND" class="button success hollow" type="submit">
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+    <div class="row column">
+      <hr>
+      <ul class="menu">
+        <li class="float-right">Copyright Footer</li>
+      </ul>
+    </div>
+
+        <script src="./assets/js/vendor/jquery.js"></script>
+        <script src="./assets/js/vendor/what-input.js"></script>
+        <script src="./assets/js/vendor/foundation.js"></script>
+        <script src="./assets/js/app.js"></script>
+    </body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html data-whatinput="keyboard" data-whatintent="keyboard" class=" whatinput-types-initial whatinput-types-keyboard"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
+        <title>PHP MVC Frameworks</title>
+            <link rel="icon" type="image/x-icon" href="favicon.ico" />
+            <link rel="stylesheet" href="./assets/css/foundation.css">
+            <link rel="stylesheet" href="./assets/css/app.css">
+    <meta class="foundation-mq"></head>
+    <body>
+
+    <!-- Start Top Bar -->
+    <div class="top-bar">
+      <div class="row">
+        <div class="top-bar-left">
+          <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
+            <li role="menuitem">PHP MVC Frameworks</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+    <br>
+    
+    <div class="row">
+      <h4>Index</h4>
+      <div class="medium-12  columns">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th width="200">Action</th>
+          </tr>
+          <tr>
+            <td>Vincent</td>
+            <td>Vega</td>
+            <td>vincent@domain.com</td>
+            <td><a class="button hollow" href="./details.html">VIEW DETAILS</a></td>
+          </tr>
+          <tr>
+            <td>Mia</td>
+            <td>Wallace</td>
+            <td>mia@domain.com</td>
+            <td><a class="button hollow" href="./details.html">VIEW DETAILS</a></td>
+          </tr>
+          <tr>
+            <td>Jules</td>
+            <td>Winnfield</td>
+            <td>jules@domain.com</td>
+            <td><a class="button hollow" href="./details.html">VIEW DETAILS</a></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+
+    <div class="row column">
+      <hr>
+      <ul class="menu">
+        <li class="float-right">Copyright Footer</li>
+      </ul>
+    </div>
+
+        <script src="./assets/js/vendor/jquery.js"></script>
+        <script src="./assets/js/vendor/what-input.js"></script>
+        <script src="./assets/js/vendor/foundation.js"></script>
+        <script src="./assets/js/app.js"></script>
+    </body>
+</html>
